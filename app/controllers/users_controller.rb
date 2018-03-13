f21c1f268827bec934cd6d0c71c78dc034945192
ingridwong0715 <ingrid830715@gmail.com>
2018-03-13 19:44:13 -0400

    Implemented login with password authentification

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,11 +20,25 @@ class UsersController < ApplicationController
 
   get '/users/:id' do
     if logged_in?
+      @user = current_user
       erb :'users/index'
     else
       redirect '/'
     end
+  end
 
+  get '/login' do
+    erb :'users/login'
+  end
+
+  post '/login' do
+    user = User.find_by(username: params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect "/users/#{user.id}"
+    else
+      redirect '/'
+    end
   end
 
 
