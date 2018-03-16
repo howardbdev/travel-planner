a8e21520943e2db174116fbd69f1f2143a701dbe
ingridwong0715 <ingrid830715@gmail.com>
2018-03-16 10:41:34 -0400

    Implemented Trip#find_by_slug

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -5,6 +5,10 @@ class Trip < ActiveRecord::Base
     "#{origin.split(', ').first}" + "-" + "#{destination.split(', ').first}" + "-" + "#{departing.month}" + "-" + "#{departing.year}" + "-id=" +"#{id}"
   end
 
+  def placeholder_value_departing
+    "#{departing.month}" + "/" + "#{departing.day}" + "/" + "#{departing.year}"
+  end
+
   def self.find_by_slug(user, slug)
     Trip.all.detect{|trip| trip.user == user && trip.slug == slug}
   end
