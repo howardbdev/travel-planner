1ef08d4ac3c1e2f91d09bf2514f270b27c8eaa9c
ingridwong0715 <ingrid830715@gmail.com>
2018-03-28 12:27:42 -0400

    added background to all logged in pages

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -2,17 +2,14 @@
 class TripsController < ApplicationController
 
   get '/my-trips/new' do
-    if logged_in?
-      erb :'trips/new'
-    else
-      redirect '/'
-    end
+    redirect_if_not_logged_in
+    erb :'trips/new'
   end
 
   post '/my-trips' do
 
   if trip = current_user.trips.find_by(origin: params[:trip][:origin], destination: params[:trip][:destination], departing: params[:trip][:departing].to_date, returning: params[:trip][:returning].to_date, transportation: params[:trip][:transportation])
-    #BUILD ALERT! 
+    #BUILD ALERT!
   else
     trip = Trip.new(params[:trip])
     trip.user = current_user
@@ -24,60 +21,47 @@ class TripsController < ApplicationController
   end
 
   get '/my-trips/:slug' do
-
-    if logged_in?
-      @trip = Trip.find_by_slug(current_user, params[:slug])
-      if @trip
-        erb :'trips/show'
-      end
-    else
-      redirect '/my-trips'
+    redirect_if_not_logged_in
+    @trip = Trip.find_by_slug(current_user, params[:slug])
+    if @trip
+      erb :'trips/show'
     end
   end
 
   get '/my-trips/:slug/edit' do
-    if logged_in?
-      @trip = Trip.find_by_slug(current_user, params[:slug])
-      if @trip
-        erb :'trips/edit'
-      end
+    redirect_if_not_logged_in
+    @trip = Trip.find_by_slug(current_user, params[:slug])
+    if @trip
+      erb :'trips/edit'
     else
-      redirect '/my-trips'
+      "You do not have this trip"
     end
+
   end
 
   patch '/my-trips/:slug' do
 
-    if logged_in?
-      @trip = Trip.find_by_slug(current_user, params[:slug])
-      if @trip
-        @trip.update(params[:trip])
-        redirect "/my-trips/#{@trip.slug}"
-      end
-    else
+    @trip = Trip.find_by_slug(current_user, params[:slug])
+    if @trip
+      @trip.update(params[:trip])
       redirect '/my-trips'
     end
+
   end
 
   delete '/my-trips/:slug' do
-    if logged_in?
-      @trip = Trip.find_by_slug(current_user, params[:slug])
-      if @trip
-        @trip.destroy
-      end
+
+    @trip = Trip.find_by_slug(current_user, params[:slug])
+    if @trip
+      @trip.destroy
     end
     redirect '/my-trips'
+
   end
 
   post '/search-trip' do
-    if logged_in?
-
-      @trips = Trip.find_by(params[:search],current_user)
-      erb :'trips/search-result'
-
-    else
-      redirect '/my-trips'
-    end
+    @trips = Trip.find_by(params[:search],current_user)
+    erb :'trips/search-result'
   end
 
 end
